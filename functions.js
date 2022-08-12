0b5aff15e9e926fdf3e7551f34d3162a8d7fc9b1
Renato F Rosa <rfrosa@LAPTOP-6TTVK6DP.localdomain>
2022-08-12 18:17:32 -0300

    adicionada imagem de fundo e js finalizado

---
+++ functions.js
@@ -0,0 +1,103 @@
+class combination {
+  id;
+  elements;
+}
+
+var elementList = [];
+var result = [];
+var stackedComb = [];
+var currentComb;
+
+function combine(position, n, p, id) {
+  // return if length of currentComb reached the value of p
+  if (currentComb.elements.length == p) {
+    return;
+  }
+  // append new element in the current combination
+  currentComb.elements.push(elementList[position]);
+  if (position == n || currentComb.elements.length == p) {
+    // reached last position or number of elements == p
+    if (currentComb.elements.length == p) {
+      result.push(JSON.parse(JSON.stringify(currentComb)));
+    }
+    return id;
+  }
+  // check if there is still elements to be combined
+  if (position + 1 < n) {
+    // stack currentComb
+    id = createUUID();
+    currentComb.id = id;
+    stackedComb.push(JSON.parse(JSON.stringify(currentComb)));
+    while (position + 1 < n) {
+      // restore previous currentComb
+      let aux = stackedComb.filter((comb) => {
+        return comb.id === id;
+      });
+      currentComb.id = aux[0].id;
+      currentComb.elements = JSON.parse(JSON.stringify(aux[0].elements));
+      position++;
+      combine(position, n, p, id);
+    }
+  }
+}
+
+function generate(aux, p) {
+  var f = document.getElementById("myForm");
+  var msg = "";
+  if (!f.checkValidity()) {
+    msg = "Os campos são de preenchimento obrigatório!";
+  }
+  elementList = aux.split(",");
+  var n = elementList.length;
+  if (n < parseInt(p)) {
+    msg = "Lista com tamanho insufiente para a combinação desejada!";
+  }
+  if (msg != "") {
+    alert(msg);
+    document.getElementById("resultados").style.display = "none";
+    document.getElementById("lista").focus();
+    return;
+  }
+  result = [];
+
+  for (m = 0; m < n; m++) {
+    // cleanup
+    currentComb = new combination();
+    currentComb.elements = [];
+    stackedComb = [];
+    // generate combinations for new pivot element
+    combine(m, n, parseInt(p), createUUID());
+  }
+
+  // output values to html
+  var linha = "";
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i].elements);
+    let aux = "";
+    for (let k = 0; k < result[i].elements.length; k++) {
+      if (aux.length > 0) aux += ", ";
+      aux += result[i].elements[k];
+    }
+    linha += "<li>" + aux + "</li>";
+  }
+  document.getElementById("combinacoes").innerHTML = linha;
+  document.getElementById("resultados").style.display = "block";
+  document.getElementById("paragraph").innerHTML =
+    "Combinações geradas - C(" + n + "," + p + ") = " + CNP(n, parseInt(p));
+}
+
+function CNP(n, p) {
+  if (n === p || p == 0) {
+    return 1;
+  } else {
+    return CNP(parseInt(n) - 1, p) + CNP(parseInt(n) - 1, parseInt(p) - 1);
+  }
+}
+
+function createUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
